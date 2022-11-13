fd40a3d39737a63e116fa9816b3a73bfaf216cb5
RichMix | Chopp_Sueyy <82078286+RichMix@users.noreply.github.com>
2022-11-13 13:21:39 -0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+export const RadioOption = ({ value, checked, onChange, children }) => { 
+ return ( 
+   <div className="RadioOption"> 
+     <input 
+       id={value} 
+       type="radio" 
+       name={value} 
+       value={value} 
+       checked={checked} 
+       onChange={(e) => { 
+         onChange(e.target.value); 
+       }} 
+     /> 
+     <label htmlFor={value}>{children}</label> 
+   </div> 
+ ); 
+}; 
